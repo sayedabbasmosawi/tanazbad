cb8a75739fa9ae8618be3737a341c788cd46dd85
sayedabbasmosawi <samosawi7@gmail.com>
2019-02-24 18:36:15 -0500

    yes

---
+++ src/test/java/Amme/DokhtarAmme.java
@@ -0,0 +1,18 @@
+package Amme;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DokhtarAmme {
+	
+	
+	@Test
+	public static void AssertTrue() {
+		
+		Assert.assertTrue(true);
+		
+		
+	} 
+	
+
+}
